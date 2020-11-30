75d4f38f1b2da15d45239df8873f47dab3427862
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2020-11-29 19:05:34 -0800

    Fix for mobile

--- src/Footer.js
+++ src/Footer.js
@@ -18,10 +18,8 @@ class Footer extends React.Component {
                     <div>< a href="mailto:feedback@wisemapping.com" > < FormattedMessage id="footer.feedback" defaultMessage="Feedback" /> </a></div>
                 </div>
                 <div>
-                    <div>
-                        <span id="siteseal" ><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=iGLOz83ePtxZCiqwz4QuXb7OQWXTflEIPdOnuR2PzaUwSru3lYJNEj9VdqJ1" > </script></span ></div>
-                        <div><span class="button-style2" >< a href="https://www.paypal.com/webapps/shoppingcart?flowlogging_id=c7ac923b53025&mfid=1606520600355_c7ac923b53025#/checkout/openButton">< FormattedMessage id="footer.donations" defaultMessage="PayPal Donations" /> </a></span > </div> 
-                    </div >
+                    <div><span class="button-style2" >< a href="https://www.paypal.com/webapps/shoppingcart?flowlogging_id=c7ac923b53025&mfid=1606520600355_c7ac923b53025#/checkout/openButton">< FormattedMessage id="footer.donations" defaultMessage="PayPal Donations" /> </a></span ></div>
+                </div >
             </footer>
         )
     };
