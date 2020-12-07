e7738b1cce63dc32950a96359106045e1192b189
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2020-12-07 11:32:57 -0800

    Minor change in title

--- packages/webapp/src/components/LoginPage.tsx
+++ packages/webapp/src/components/LoginPage.tsx
@@ -77,7 +77,7 @@ const LoginForm = () => {
 const LoginPage = (props: any) => {
 
   useEffect(() => {
-    document.title = 'Login - WiseMapping';
+    document.title = 'Login | WiseMapping';
   });
 
   return (
