9c9d9ed9c21a54791b8ae92de6b1a858672bdd1b
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2021-02-17 00:11:08 -0800

    Add html index.html generation

--- packages/webapp/webpack.dev.js
+++ packages/webapp/webpack.dev.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
--- packages/webapp/webpack.prod.js
+++ packages/webapp/webpack.prod.js
@@ -1,9 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
@@ -13,14 +11,13 @@ module.exports = merge(common, {
     minimize: true
   },
   plugins: [
-    new CompressionPlugin()
-    // ,
-    // new HtmlWebpackPlugin({
-    //   template: path.join(__dirname, 'public/index.html'),
-    //   templateParameters: {
-    //     PUBLIC_URL: process.env.PUBLIC_URL ? process.env.PUBLIC_URL : 'https://www.wisemapping.com'
-    //   },
-    //   base: process.env.PUBLIC_URL ? process.env.PUBLIC_URL : 'https://www.wisemapping.com'
-    // })
+    new CompressionPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+      templateParameters: {
+        PUBLIC_URL: process.env.PUBLIC_URL ? process.env.PUBLIC_URL : 'https://www.wisemapping.com'
+      },
+      base: process.env.PUBLIC_URL ? process.env.PUBLIC_URL : 'https://www.wisemapping.com'
+    })
   ]
 });
