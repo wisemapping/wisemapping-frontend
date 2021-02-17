fca3b555519cbe074f8c9bbfa77ffad97019def9
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2021-02-16 23:50:39 -0800

    chores: compile

--- packages/webapp/webpack.prod.js
+++ packages/webapp/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 const CompressionPlugin = require('compression-webpack-plugin');
