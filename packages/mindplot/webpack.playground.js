a96c6be2529aa001e4e4ed58aaacdead135d8d82
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2021-10-04 17:38:11 -0700

    Create libraries directory

--- packages/mindplot/webpack.playground.js
+++ packages/mindplot/webpack.playground.js
@@ -4,43 +4,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    entry: {
-        layout: path.resolve(__dirname, './test/playground/context-loader'),
+  entry: {
+    layout: path.resolve(__dirname, './test/playground/context-loader'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist', 'test'),
+    filename: '[name].test.js',
+    publicPath: '',
+  },
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 2000000,
     },
-    output: {
-        path: path.resolve(__dirname, 'dist', 'test'),
-        filename: '[name].test.js',
-        publicPath: '',
-    },
-    mode: 'production',
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            minSize: 2000000,
-        },
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                test: /.(js|jsx)$/,
-                exclude: /node_modules/,
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.js','.json'],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'test/playground/index.html',
-        }),
-        new HtmlWebpackPlugin({
-            chunks: ['layout'],
-            filename: 'layout',
-            template: 'test/playground/layout.html',
-        }),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /.js$/,
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'test/playground/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ['layout'],
+      filename: 'layout',
+      template: 'test/playground/layout.html',
+    }),
+  ],
 };
--- packages/web2d/webpack.common.js
+++ packages/web2d/webpack.common.js
@@ -23,4 +23,5 @@ module.exports = {
     extensions: ['.js', '.json'],
   },
   plugins: [new CleanWebpackPlugin()],
+  
 };
