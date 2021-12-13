811955f0809da67b4098fc03dc7fd2427ffbfb76
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2021-12-13 13:46:50 -0800

    Fix missing declaration on LinkEditor

--- packages/mindplot/src/components/widget/LinkEditor.js
+++ packages/mindplot/src/components/widget/LinkEditor.js
@@ -15,6 +15,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+import { $assert } from '@wisemapping/core-js';
 import BootstrapDialog from '../libraries/bootstrap/BootstrapDialog';
 
 class LinkEditor extends BootstrapDialog {/** @lends LinkEditor */
