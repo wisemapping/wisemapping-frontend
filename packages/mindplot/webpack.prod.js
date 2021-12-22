c36ea0e38702baa700c0bcf7514a297946bd6c68
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2021-12-21 20:38:13 -0800

    Add compression pluggin

--- packages/mindplot/webpack.prod.js
+++ packages/mindplot/webpack.prod.js
@@ -1,15 +1,22 @@
 const { merge } = require('webpack-merge');
+const CompressionPlugin = require('compression-webpack-plugin');
 const common = require('./webpack.common');
 
 const prodConfig = {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
+    minimize: true,
     splitChunks: {
       chunks: 'all',
       minSize: 2000000,
     },
   },
+  plugins: [
+    new CompressionPlugin({
+      test: /\.js(\?.*)?$/i
+    }),
+  ],
 };
 
 module.exports = merge(common, prodConfig);
