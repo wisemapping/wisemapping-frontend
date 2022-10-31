9c80d0cfd32723e38bed290cd241f01c3bc1ff6d
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2022-10-30 23:16:09 -0700

    Migration to cypress 10.

---
+++ packages/editor/cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  video: false,
+  videoUploadOnPasses: false,
+  includeShadowDom: true,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:8081',
+  },
+});
