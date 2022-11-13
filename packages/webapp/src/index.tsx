bcb871e5b85b6dacc3fa25da0444dd6689a6525d
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2022-11-12 18:42:52 -0800

    Add why-did-you-render plugin.

--- packages/webapp/src/index.tsx
+++ packages/webapp/src/index.tsx
@@ -1,9 +1,11 @@
+import './wdyr'; // <--- first import
+
 import React from 'react';
 import App from './app';
 import { createRoot } from 'react-dom/client';
 
 async function bootstrapApplication() {
-  const container = document.getElementById('root') as HTMLElement
+  const container = document.getElementById('root') as HTMLElement;
   const root = createRoot(container!);
   root.render(<App />);
 }
