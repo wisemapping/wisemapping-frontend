e70090cf25eb3b733002c6b648e4dead99815344
Clement Creusat <clement.creusat@gmail.com>
2023-06-27 16:19:03 +0200

    fix(deps): clean deps/peer-deps and webpack conf external

--- packages/mindplot/webpack.prod.js
+++ packages/mindplot/webpack.prod.js
@@ -1,11 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const pkg = require('./package.json');
 
 const prodConfig = {
   optimization: {
     usedExports: true,
     minimize: true,
   },
+  externals: [...Object.keys(pkg.dependencies)],
 };
 
 module.exports = merge(common, prodConfig);
