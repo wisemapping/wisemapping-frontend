5cb0782f6417972e21a680bf48fdb9e02a580f1a
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2023-07-31 22:56:25 -0700

    Fix mindplot inclusion of web2d.

--- packages/mindplot/webpack.prod.js
+++ packages/mindplot/webpack.prod.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
-const pkg = require('./package.json');
+// const pkg = require('./package.json');
 
 const prodConfig = {
   optimization: {
     usedExports: true,
     minimize: true,
   },
-  externals: [...Object.keys(pkg.dependencies)],
+  // externals: [...Object.keys(pkg.dependencies)],
 };
 
 module.exports = merge(common, prodConfig);
