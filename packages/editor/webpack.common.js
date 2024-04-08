ae11c6a45992d117e6e38d37f1a0a1e084ddf7d3
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2024-04-07 23:08:30 -0700

    Clean up configurations.

--- packages/editor/webpack.common.js
+++ packages/editor/webpack.common.js
@@ -7,18 +7,14 @@ const prodConfig = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: '',
     library: {
       type: 'umd',
     },
   },
   stats: {
     errorDetails: true,
-  }, entry: {
-    'editor.bundle': path.join(__dirname, 'src', 'index.tsx'),
   },
   mode: 'development',
-  target: 'web',
 };
 
 module.exports = merge(common, prodConfig);
