ba3cca2c44f679cad36d722a12ea926cf7c7441d
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2025-09-09 16:25:50 -0300

    Upgrade storybook 7.

--- packages/web2d/.storybook/main.js
+++ packages/web2d/.storybook/main.js
@@ -1,15 +1,42 @@
 module.exports = {
-  "stories": [
-    "../storybook/src/**/*.stories.mdx",
-    "../storybook/src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../storybook/src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/html",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: {
+    name: "@storybook/html-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: "tag"
+  },
+  babel: async (options) => ({
+    ...options,
+    presets: [
+      ...(options.presets || []),
+      ["@babel/preset-typescript", { allowDeclareFields: true }]
+    ]
+  }),
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-typescript', { allowDeclareFields: true }]
+            ]
+          }
+        }
+      ]
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
   }
 }
