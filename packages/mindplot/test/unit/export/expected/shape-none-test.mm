bf6a3ddfb5fa902407ff669433d0f0c3c8850792
Paulo Gustavo Veiga <pveiga@wisemapping.com>
2025-10-21 13:50:10 -0300

    Fix failing tests

---
+++ packages/mindplot/test/unit/export/expected/shape-none-test.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+    <node ID="ID_1" TEXT="Central Topic">
+        <node ID="ID_2" POSITION="right" STYLE="fork" TEXT="Topic with undefined shape (uses theme default)"/>
+        <node ID="ID_3" POSITION="right" STYLE="none" TEXT="Topic with explicit none shape"/>
+        <node ID="ID_4" POSITION="right" TEXT="Topic with line shape"/>
+        <node ID="ID_5" POSITION="right" STYLE="rectangle" TEXT="Topic with rectangle shape"/>
+    </node>
+</map>
